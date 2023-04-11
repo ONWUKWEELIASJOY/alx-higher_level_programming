a30ac1f2e50cef8c2f73ecc205251203229594a0
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-11 08:59:24 -0700

    changes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,37 +1,12 @@
 #!/usr/bin/python3
-"""
-locked_class.py
-This module defines a LockedClass that allows only the 'first_name' attribute to be set.
 
-locked_obj = LockedClass()
-locked_obj.first_name = 'John'  # Allowed
-locked_obj.last_name = 'Doe'    # Raises AttributeError
+class LockedMeta(type):
+    def __new__(cls, name, bases, dct):
+	dct['__slots__'] = ('first_name',)
+	return
+super().__new__(cls, name, bases, dct)
 
-"""
 
-class LockedClass:
-    """
-    A locked class that allows only the 'first_name' attribute to be set.
-    """
-   
-    def __setattr__(self, name, value):
-    """
-Sets the value of the specified attribute, if it is allowed.
-    
-    Args:
-	name (str): The name of the attribute to set.
-        value (any): The value to set the attribute to.
-        
-    Raises:
-AttributeError: If the specified attribute is not allowed to be set.
-      """
-      if name == 'first_name':
-object.__setattr__(self, name, value)
-                                 elif hasattr(self, name):
-object.__setattr__(self, name, value)
-	                         else:
-				     raise AttributeError(f"'{name}' attribute is locked and cannot be set")
-
-"""
-An OOP task on alx,)
-"""
+class LockedClass(metaclass=LockedMeta):
+    """ locked class """
+    pass
