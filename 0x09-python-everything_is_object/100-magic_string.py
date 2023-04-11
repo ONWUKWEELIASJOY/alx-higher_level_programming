4062eae5edc8cf8d9194b41f9f9110b949e38e08
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-11 06:12:39 -0700

    my obj files;OOP

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_string(List_of_str=None):
+ if List_of_str is None:
+    List_of_str = []
+ List_of_str.append("BestSchool")
+ return ", ".join(List_of_str)
+
+"""
+AN OOP TASK ON ALX
+"""
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class LockedClass:
+  """ locked class """
+  def __setattr__(self, name, value):
+    if not hasattr(self, name):
+       object.__setattr__(self, name, value)
+    else:
+      raise AttributeError(f"{name} attribute is locked and cannot be set")
+
+# available slots that can be created
+__slots__ = ('first_name',)
+
+"""
+An OOP task on alx,)
+"""
