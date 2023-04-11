cc17371f3370a2de49e09ba02c909fa28482a478
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-11 07:42:48 -0700

    changes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string(List_of_str=None):
- if List_of_str is None:
-    List_of_str = []
- List_of_str.append("BestSchool")
- return ", ".join(List_of_str)
-
-"""
-AN OOP TASK ON ALX
-"""
-
+magic_string = lambda List_of_str=[]: ",
+".join(List_of_str + ["BestSchool"])
+print(magic_string())
