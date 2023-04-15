db567ffe9f27e6b6b75da8455424a4b6d9b26f47
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-15 14:39:11 -0700

    print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,11 @@
  #!/usr/bin/python3
 
+"""safe printing integer task1"""
+
 def safe_print_integer(value):
     try:
 	print("{:d}".format(value))
-        return True
-    except:
+	return True
+    else:
+    except (ValueError, TypeError):
         return False
