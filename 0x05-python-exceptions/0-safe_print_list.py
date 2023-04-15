053c999b91352b61b3ecea4ba91f1e324a5e9e4b
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-15 13:44:12 -0700

    prints

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Safe printing"""
-afe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
 
     count = 0
     for i in range(x):
@@ -10,5 +10,5 @@ afe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-    print()
+    print("")
     return count
