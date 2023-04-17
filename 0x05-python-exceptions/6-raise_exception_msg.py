6334422847325f01599a5cfc542397c83cfed916
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-17 04:41:09 -0700

    tasks

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -3,7 +3,4 @@
 """exception message"""
 
 def raise_exception_msg(message=""):
-    try:
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
+    raise NameError(message)
