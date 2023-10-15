d57ce04eabd813a1651b1dc2cbbc89467b12795b
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-10-15 13:37:08 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""
-  This function takes an object as input and returns a list of attributes and methods available for the object.
-"""
-def lookup(obj):
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or '__' not in attr]
