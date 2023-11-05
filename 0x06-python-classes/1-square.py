030a3d1bd6eb9e7690ea446f9718de9d3afc16ee
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-11-05 02:45:06 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 0-square.py by Elias joy chinonso
+
+"""gives a square """
+
+
+class Square:
+    """shows a square"""
+
+    def __init__(self, size):
+        """Initializing square class
+        Args: size - shows size of the square defined
+        """
+
+        self.__size = size
