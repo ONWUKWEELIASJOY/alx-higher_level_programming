587cc8edabd2778ae6b4a502c2f7bdda9f486cf5
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-11-05 02:49:29 -0800

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 0-square.py by Elias joy chinonso
+
+"""module that defines a square """
+
+
+class Square:
+    """class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: shows size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
